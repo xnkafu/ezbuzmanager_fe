f91b42b94c038e4703a3308de87b0634eca26ec8
xnkafu <xnkafu@gmail.com>
2023-06-02 19:19:03 -0700

    updated backend links

--- src/config/url.js
+++ src/config/url.js
@@ -1,1 +1,3 @@
-export let url = 'https://8442-2601-43-8201-4e30-fc5e-21b6-9a9c-94c8.ngrok-free.app'
+//export let url = 'https://8442-2601-43-8201-4e30-fc5e-21b6-9a9c-94c8.ngrok-free.app'
+
+export let url = 'http://localhost:8080'
