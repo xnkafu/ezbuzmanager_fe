5d2d7dc441b881b1a924e35c14e67118eba07f88
xnkafu <xnkafu@gmail.com>
2023-07-06 20:49:02 -0700

    reverse sales and display the latest first

--- src/config/url.js
+++ src/config/url.js
@@ -1,3 +1,3 @@
-export let url = 'https://6814-2601-43-8201-4e30-fc5e-21b6-9a9c-94c8.ngrok-free.app'
+export let url = 'https://012e-2601-43-8201-4e30-fc5e-21b6-9a9c-94c8.ngrok-free.app'
 
 //export let url = 'https://10.0.0.175:8080'
